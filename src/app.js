f67d1047514f0dd38441264435116c683648192a
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-05-11 11:50:44 +0700

    Init

---
+++ src/app.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const obfuscator = require("javascript-obfuscator");
+const format = require("json-beautify");
+
+app.get("/", (_req, res) => {
+  res.send(
+    format({
+      error: false,
+      status: res.statusCode,
+      endpoint: "api/obfuscate",
+      query: {
+        required: "?code=",
+      },
+    })
+  );
+
+  console.log({
+    error: false,
+    status: res.statusCode,
+    endpoint: "api/obfuscate",
+    query: {
+      required: "?code=",
+    },
+  });
+});
+
+app.get("/api/obfuscate", (req, res) => {
+  const codeParams = req.query.code;
+  if (!codeParams)
+    return res.send({
+      error: true,
+      message: "Code Parameter / Query must be provided",
+      codeParams: codeParams || "",
+    });
+
+  try {
+    res.send({
+      error: false,
+      status: res.statusCode,
+      codeParams,
+      result: obfuscator.obfuscate(codeParams),
+    });
+  } catch (err) {
+    return res.send({
+      error: true,
+      codeParams,
+      message: `${err}`,
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
